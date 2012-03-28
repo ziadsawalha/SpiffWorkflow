5fd13695faf453c2afa384de181f4b821c05bebb
Ziad Sawalha <github@highbridgellc.com>
2012-03-28 05:17:38 -0500

    Empty outer_job error in debug
    - handle correctly when outer_job is None when debug is set to True

--- src/SpiffWorkflow/specs/TaskSpec.py
+++ src/SpiffWorkflow/specs/TaskSpec.py
@@ -357,7 +357,8 @@ class TaskSpec(object):
         my_task.workflow._task_completed_notify(my_task)
 
         if my_task.workflow.debug:
-            my_task.workflow.outer_job.task_tree.dump()
+            if hasattr(my_task.workflow, "outer_job"):
+                my_task.workflow.outer_job.task_tree.dump()
 
         self.completed_event.emit(my_task.workflow, my_task)
         return True
